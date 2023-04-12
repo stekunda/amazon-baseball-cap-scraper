b257f35bdd9c12e192b14a3067471e69f82ee2c7
Stekunda Henry <stekundah@gmail.com>
2023-04-12 09:04:35 -0400

    Added pagination scraping functionality

--- index.js
+++ index.js
@@ -11,84 +11,70 @@ const scrapeResults = [
     },
 ];
 
-(async () => {
-    const link =
-        "https://www.amazon.com/s?k=baseball+caps&crid=17SO003E1ZBDD&sprefix=baseball+caps%2Caps%2C118&ref=nb_sb_noss_1";
+let results = [];
 
+async function scrape() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    await page.goto(link);
+    await page.goto(
+        "https://www.amazon.com/s?k=baseball+caps&crid=17SO003E1ZBDD&qid=1681236643&sprefix=baseball+caps%2Caps%2C118&ref=sr_pg_"
+    );
 
-    const html = await page.content();
-    const $ = cheerio.load(html);
+    while (
+        await page.$(
+            ".s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator"
+        )
+    ) {
+        const html = await page.content();
+        await sleep(1000); // 1 second sleep
+        const $ = cheerio.load(html);
 
-    //$(".a-price").each((index, element) => {
-    //extract the price only
-    //var priceArray = $(element).text().split("$");
-    //var price = priceArray[1];
-    //console.log("Price: " + price);
-    //});
+        // using cheerio to scrape the necessary data
+        $(".a-section.a-spacing-base.a-text-center")
+            .map((index, element) => {
+                const nameElement = $(element).find(
+                    ".a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2"
+                );
+                const brandElement = $(element).find(".s-line-clamp-1");
+                const priceElement = $(element).find(".a-price");
+                const urlElement = $(element).find(
+                    ".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal"
+                );
+                const ratingElement = $(element).find(".a-row.a-size-small");
+                const imgElement = $(element).find(".s-image");
 
-    // cent price
-    // $(".a-price-fraction").each((index, element) =>
-    //     console.log($(element).text())
-    // );
+                const name = $(nameElement).text();
+                const brand = $(brandElement).text();
+                const priceArray = $(priceElement).text().split("$");
+                const price = priceArray[1];
+                const url = "amazon.com" + $(urlElement).attr("href");
+                const rating = $(ratingElement).text().slice(0, 3);
+                const image = $(imgElement).attr("src");
 
-    // $(
-    //     ".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal"
-    // ).each((index, element) => {
-    //     // begin each url with "amazon.com"
-    //     // var url = "amazon.com" + $(element).attr("href");
-    //     //console.log(url, "\n");
-    // });
+                const item = { name, brand, price, rating, image, url };
+                results.push(item);
+            })
+            .get();
 
-    // The brand
-    // $(".s-line-clamp-1").each((index, element) => {
-    //     var brand = $(element).text();
-    //     console.log("Brand: " + brand);
-    // });
-
-    // The name of the item
-    // $(".a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2").each(
-    //     (index, element) => {
-    //         var name = $(element).text();
-    //         console.log("Name: " + name);
-    //     }
-    // );
-
-    // $(".a-row.a-size-small").each((index, element) => {
-    //     // Extract just the first 3 characters (4.7, 3.8, etc)
-    //     var rating = $(element).text().slice(0, 3);
-    //     //console.log("Rating: " + rating);
-    // });
-
-    const results = $(".a-section.a-spacing-base.a-text-center")
-        .map((index, element) => {
-            const nameElement = $(element).find(
-                ".a-size-mini.a-spacing-none.a-color-base.s-line-clamp-2"
-            );
-            const brandElement = $(element).find(".s-line-clamp-1");
-            const priceElement = $(element).find(".a-price");
-            const urlElement = $(element).find(
-                ".a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal"
-            );
-            const ratingElement = $(element).find(".a-row.a-size-small");
-            const imgElement = $(element).find(".s-image");
-
-            const name = $(nameElement).text();
-            const brand = $(brandElement).text();
-            const priceArray = $(priceElement).text().split("$");
-            const price = priceArray[1];
-            const url = "amazon.com" + $(urlElement).attr("href");
-            const rating = $(ratingElement).text().slice(0, 3);
-            const image = $(imgElement).attr("src");
-
-            return { name, brand, price, rating, image, url };
-        })
-        .get();
-
+        // clicking the next button to deal with pagination scraping
+        await page.click(
+            ".s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator"
+        );
+        await page
+            .waitForSelector(
+                ".s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator",
+                { timeout: 2000 } // increase timeout to 10 seconds
+            )
+            .catch(() => {}); // catch timeout error and continue
+    }
     console.log(results);
-
     await browser.close();
-})();
+}
+
+// adding a delay
+async function sleep(miliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, miliseconds));
+}
+
+scrape();
